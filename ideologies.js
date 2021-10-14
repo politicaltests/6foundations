4601034b13ad5962ef7f8ced5eb609a004220ef1
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-10-14 22:26:20 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -4,38 +4,38 @@ ideologies = [
  "link": "https://en.wikipedia.org/wiki/Progressivism",
  "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
  "next": "Social Democracy, Left-Wing Populism, Liberalism",
- "stats": { "care": 75, "fairness": 75, "loyalty": 35, "authority": 50, "sanctity": 20, "liberty": 42 }},
+ "stats": { "care": 100, "fairness": 75, "loyalty": 35, "authority": 50, "sanctity": 20, "liberty": 42 }},
  
  { "name": "Social Democracy",
  "link": "https://en.wikipedia.org/wiki/Social_democracy",
  "desc": "Social Democracy advocates for economic and social interventions to promote social justice within the framework of a liberal-democratic polity and a capitalist-oriented mixed economy.",
  "next": "Democratic Socialism, Christian Democracy, Left-Wing Populism",
- "stats": { "care": 65, "fairness": 80, "loyalty": 35, "authority": 50, "sanctity": 20, "liberty": 35 }},
+ "stats": { "care": 85, "fairness": 80, "loyalty": 35, "authority": 50, "sanctity": 20, "liberty": 35 }},
  
  { "name": "Christian Democracy",
  "link": "https://en.wikipedia.org/wiki/Christian_democracy",
  "desc": "Christian democracy is a political ideology based on modern democratic ideas, socialist ethics, and traditional Christian values. It tends to be Center-Left in terms of economic and foreign policy, but tends to be Center-Right in terms of social issues.",
  "next": "Social Democracy, Progressive Conservatism, Left-Wing Populism",
- "stats": { "care": 55, "fairness": 70, "loyalty": 45, "authority": 55, "sanctity": 60, "liberty": 35 }},
+ "stats": { "care": 65, "fairness": 70, "loyalty": 45, "authority": 55, "sanctity": 60, "liberty": 35 }},
  
  { "name": "Progressive Conservatism",
  "link": "https://en.wikipedia.org/wiki/Progressive_conservatism",
  "desc": "Progressive conservatism is a political ideology which combines conservative and progressive policies. It believes that social reform and progression in areas such as science, economics, education, technology and medicine were necessary to improve human living conditions, but also supports the preservation of tradition.",
  "next": "Christian Democracy, Conservatism, Progressivism",
- "stats": { "care": 60, "fairness": 70, "loyalty": 45, "authority": 55, "sanctity": 50, "liberty": 45 }},
+ "stats": { "care": 70, "fairness": 70, "loyalty": 45, "authority": 55, "sanctity": 50, "liberty": 45 }},
  
  //Fairness Oriented
  { "name": "Democratic Socialism",
  "link": "https://en.wikipedia.org/wiki/Democratic_socialism",
  "desc": "Democratic socialism is a left-wing political philosophy that supports political democracy within a socially owned economy, with a particular emphasis on economic democracy, workplace democracy and workers' self-management within a market socialist economy, or an alternative form of a decentralised planned socialist economy.",
  "next": "Social Democracy, Left-Wing Populism, Progressivism",
- "stats": { "care": 70, "fairness": 90, "loyalty": 40, "authority": 60, "sanctity": 20, "liberty": 40 }},
+ "stats": { "care": 80, "fairness": 90, "loyalty": 40, "authority": 60, "sanctity": 20, "liberty": 40 }},
  
  { "name": "Left-Wing Populism",
  "link": "https://en.wikipedia.org/wiki/Left-wing_populism",
  "desc": "Left-Wing Populism is a political ideology that combines left-wing politics with populist rhetoric and themes. Recurring themes for left-wing populists include economic democracy, social justice, and skepticism of globalization.",
  "next": "Social Democracy, Left-Wing Populism, Progressivism",
- "stats": { "care": 65, "fairness": 85, "loyalty": 45, "authority": 60, "sanctity": 20, "liberty": 65 }},
+ "stats": { "care": 75, "fairness": 85, "loyalty": 45, "authority": 60, "sanctity": 20, "liberty": 65 }},
  
  { "name": "State Socialism",
  "link": "https://en.wikipedia.org/wiki/State_socialism",
@@ -43,6 +43,7 @@ ideologies = [
  "next": "Marxism-Leninism, Left-Wing Populism, Democratic Socialism",
  "stats": { "care": 60, "fairness": 95, "loyalty": 50, "authority": 70, "sanctity": 40, "liberty": 30 }},
  
+ //Fairness-Authority Oriented
  { "name": "Marxism-Leninism",
  "link": "https://en.wikipedia.org/wiki/Marxism-Leninism",
  "desc": "Marxist–Leninists support proletarian internationalism and socialist democracy, and oppose anarchism, fascism, imperialism, and liberal democracy. Marxism–Leninism holds that a two-stage communist revolution is needed to replace capitalism.",
@@ -59,7 +60,38 @@ ideologies = [
  "link": "https://en.wikipedia.org/wiki/Stalinism",
  "desc": "National Bolshevism advocates for a Bolshevik-inspired system of governance, which includes a socialist economy and a strong centralised state, in combination with extreme nationalism and support for traditional values, borrowing some elements and aesthetics from fascism.",
  "next": "Marxism-Leninism, State Socialism, Fascism",
- "stats": { "care": 30, "fairness": 100, "loyalty": 80, "authority": 100, "sanctity": 100, "liberty": 0 }},
+ "stats": { "care": 30, "fairness": 100, "loyalty": 80, "authority": 100, "sanctity": 90, "liberty": 0 }},
+ 
+ //Fairness-Liberty Oriented
+ { "name": "Council Communism",
+ "link": "https://en.wikipedia.org/wiki/Council_communism",
+ "desc": "Council communism is a current of communist thought that emerged in the 1920s. Inspired by the November Revolution, council communism was opposed to state socialism and advocated workers' councils and council democracy.",
+ "next": "Libertarian Socialism, Democratic Socialism, Libertarian Market Socialism",
+ "stats": { "care": 70, "fairness": 90, "loyalty": 45, "authority": 40, "sanctity": 25, "liberty": 65 }},
+ 
+ { "name": "Libertarian Socialism",
+ "link": "https://en.wikipedia.org/wiki/Libertarian_socialism",
+ "desc": "Libertarian Socialism is an anti-authoritarian, anti-statist and libertarian political philosophy within the socialist movement which rejects the state socialist conception of socialism as a statist form where the state retains centralized control of the economy.",
+ "next": "Libertarian Market Socialism, Council Communism, Anarcho-Communism",
+ "stats": { "care": 70, "fairness": 90, "loyalty": 25, "authority": 30, "sanctity": 25, "liberty": 80 }},
+ 
+ { "name": "Libertarian Market Socialism",
+ "link": "https://en.wikipedia.org/wiki/Libertarian_socialism",
+ "desc": "Libertarian Market Socialism is an anti-authoritarian, anti-statist and libertarian political philosophy that combines Libertarian Socialism with Market Socialism, which is a type of economic system involving the public, cooperative, or social ownership of the means of production in the framework of a market economy.",
+ "next": "Libertarian Market Socialism, Council Communism, Anarcho-Communism",
+ "stats": { "care": 50, "fairness": 70, "loyalty": 25, "authority": 30, "sanctity": 25, "liberty": 85 }},
+ 
+ { "name": "Anarcho-Communism",
+ "link": "https://en.wikipedia.org/wiki/Libertarian_socialism",
+ "desc": "Anarcho-Communism is a political philosophy and anarchist school of thought which advocates the abolition of the state, capitalism, wage labour, social hierarchies and private property.",
+ "next": "Anarcho-Mutualism, Libertarian Socialism, Libertarian Market Socialism",
+ "stats": { "care": 70, "fairness": 90, "loyalty": 25, "authority": 0, "sanctity": 25, "liberty": 100 }},
+ 
+ { "name": "Anarcho-Mutualism",
+ "link": "https://en.wikipedia.org/wiki/Libertarian_socialism",
+ "desc": "Anarcho-Mutualism is an anarchist school of thought and economic theory that advocates a socialist society based on free markets and usufructs, i.e. occupation and use property norms. One implementation of this system involves the establishment of a mutual-credit bank that would lend to producers at a minimal interest rate, just high enough to cover administration.",
+ "next": "Anarcho-Communism, Anarcho-Capitalism, Libertarian Market Socialism",
+ "stats": { "care": 50, "fairness": 70, "loyalty": 25, "authority": 0, "sanctity": 25, "liberty": 100 }},
  
  //Sanctity Oriented
  { "name": "Conservatism",
@@ -72,38 +104,50 @@ ideologies = [
  { "name": "Right-Libertarianism",
  "link": "https://en.wikipedia.org/wiki/Right-libertarianism",
  "desc": "Right-Libertarianism is a political philosophy and movement that upholds liberty as a core principle. Right-Libertarians seek to maximize autonomy and political freedom, emphasizing free association, free markets, individualism and voluntary association.",
- "next": "Bleeding Heart Libertarianism, Paleolibertarianism, Anarcho-Capitalism",
+ "next": "Bleeding Heart Libertarianism, Paleolibertarianism, Minarchism, Objectivism",
  "stats": { "care": 35, "fairness": 42, "loyalty": 25, "authority": 30, "sanctity": 40, "liberty": 85 }},
  
  { "name": "Bleeding-Heart Libertarianism",
  "link": "https://en.wikipedia.org/wiki/Neoclassical_liberalism",
  "desc": "Bleeding-Heart Libertarianism is a libertarian political philosophy that focuses on the support for civil liberties and free markets and the concern for social justice and the well-being of the disadvantaged.",
- "next": "Right-Libertarianism, Paleolibertarianism, Anarcho-Capitalism",
+ "next": "Right-Libertarianism, Paleolibertarianism, Minarchism, Objectivism",
  "stats": { "care": 45, "fairness": 42, "loyalty": 25, "authority": 30, "sanctity": 20, "liberty": 85 }},
  
  { "name": "Paleolibertarianism",
  "link": "https://en.wikipedia.org/wiki/Paleolibertarianism",
  "desc": "Paleolibertarianism is a variant of Libertarianism which stresses inherent incompatibility between progressivism and the concept of liberty, as well as a focus on the importance of inherited culture as a means of maintaining order.",
- "next": "Right-Libertarianism, Bleeding Heart Libertarianism, Anarcho-Capitalism",
+ "next": "Right-Libertarianism, Bleeding Heart Libertarianism, Minarchism, Objectivism",
  "stats": { "care": 35, "fairness": 40, "loyalty": 25, "authority": 30, "sanctity": 70, "liberty": 85 }},
+ 
+ { "name": "Minarchism",
+ "link": "https://en.wikipedia.org/wiki/Night-watchman_state",
+ "desc": "Minarchism advocates for a state that acts only as an enforcer of the non-aggression principle by providing citizens with the military, the police, and courts, thereby protecting them from aggression, theft, breach of contract, fraud, and enforcing property laws.",
+ "next": "Anarcho-Capitalism, Objectivism, Right-Libertarianism",
+ "stats": { "care": 35, "fairness": 40, "loyalty": 25, "authority": 10, "sanctity": 40, "liberty": 92 }},
  
  { "name": "Anarcho-Capitalism",
  "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism",
  "desc": "Anarcho-Capitalism is a political philosophy and economic theory that advocates the elimination of centralized states in favor of a system of private property enforced by private agencies, free markets and the right-libertarian interpretation of self-ownership, which extends the concept to include control of private property as part of the self.",
- "next": "Anarcho-Capitalism, Paleolibertarianism, Bleeding Heart Libertarianism",
+ "next": "Anarcho-Capitalism, Paleolibertarianism, Bleeding Heart Libertarianism, Minarchism",
  "stats": { "care": 35, "fairness": 40, "loyalty": 25, "authority": 0, "sanctity": 40, "liberty": 100 }},
  
- //Duplicate Ideologies for more accuracy 
- /* { "name": "Anarcho-Capitalism", //Broader "Sanctity" scores
- "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism",
- "desc": "Anarcho-Capitalism is a political philosophy and economic theory that advocates the elimination of centralized states in favor of a system of private property enforced by private agencies, free markets and the right-libertarian interpretation of self-ownership, which extends the concept to include control of private property as part of the self.",
- "next": "Anarcho-Capitalism, Paleolibertarianism, Bleeding Heart Libertarianism",
- "stats": { "care": 35, "fairness": 40, "loyalty": 25, "authority": 0, "sanctity": 70, "liberty": 100 }},
+ { "name": "Objectivism",
+ "link": "https://en.wikipedia.org/wiki/Right-libertarianism",
+ "desc": "Right-Libertarianism is a political philosophy and movement that upholds liberty as a core principle. Right-Libertarians seek to maximize autonomy and political freedom, emphasizing free association, free markets, individualism and voluntary association.",
+ "next": "Bleeding Heart Libertarianism, Paleolibertarianism, Anarcho-Capitalism",
+ "stats": { "care": 15, "fairness": 10, "loyalty": 25, "authority": 30, "sanctity": 40, "liberty": 85 }},
  
- { "name": "Anarcho-Capitalism", //Broader "Sanctity" scores
- "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism",
- "desc": "Anarcho-Capitalism is a political philosophy and economic theory that advocates the elimination of centralized states in favor of a system of private property enforced by private agencies, free markets and the right-libertarian interpretation of self-ownership, which extends the concept to include control of private property as part of the self.",
- "next": "Anarcho-Capitalism, Paleolibertarianism, Bleeding Heart Libertarianism",
- "stats": { "care": 35, "fairness": 40, "loyalty": 25, "authority": 0, "sanctity": 20, "liberty": 100 }}, */
-
+ //Loyalty-Authority Oriented
+ { "name": "Fascism",
+ "link": "https://en.wikipedia.org/wiki/Fascism",
+ "desc": "Fascism is a form of totalitarian ultranationalism characterized by dictatorial power, the prioritization of the nation above all else, and the strong regimentation of society and of the economy.",
+ "next": "National Socialism, National Bolshevism, Stalinism",
+ "stats": { "care": 0, "fairness": 0, "loyalty": 90, "authority": 100, "sanctity": 90, "liberty": 0 }},
+ 
+ { "name": "National Socialism",
+ "link": "https://en.wikipedia.org/wiki/Nazism",
+ "desc": "National Socialism, often shortened as Nazism, is a totalitarian ideology that has disdain for liberal democracy and the parliamentary system, and advocates for ideas like antisemitism, anti-communism, scientific racism, German nationalism, and eugenics.",
+ "next": "Fascism, National Bolshevism, Stalinism",
+ "stats": { "care": 0, "fairness": 0, "loyalty": 90, "authority": 100, "sanctity": 90, "liberty": 0 }},
+ 
 ];
